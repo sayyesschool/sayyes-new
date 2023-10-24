772a90e38e9e5b281e04121c118a0c086388d091
Andrey <raigon28@gmail.com>
2023-10-24 19:20:50 +0300

    feat: add tabs

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,11 @@
 import { header } from "./header.js";
 import { modals } from "./modals.js";
 import { accordion } from "./accordion.js";
+import { tabs } from "./tabs.js";
 
 console.log(111, "Hello world!");
 
 header();
 modals();
 accordion();
+tabs();
