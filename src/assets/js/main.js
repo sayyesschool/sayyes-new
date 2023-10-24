a2e2377a318abb34d2c928eb246859d1f9bc6041
Andrey <raigon28@gmail.com>
2023-10-24 07:58:05 +0300

    feat: add accordion

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,9 @@
 import { header } from "./header.js";
 import { modals } from "./modals.js";
+import { accordion } from "./accordion.js";
 
 console.log(111, "Hello world!");
 
 header();
 modals();
+accordion();
