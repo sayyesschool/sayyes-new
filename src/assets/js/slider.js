b2b65869419533232dd5531c322970d12e9a4d37
Andrey <raigon28@gmail.com>
2023-12-20 03:00:20 +0300

    feat: adaptive for organizations

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -8,10 +8,25 @@ export const slider = () => {
 		pagination: {
 			el: ".slider__pagination",
 		},
-		// breakpoints: {
-		// 	1300: {
-		// 		spaceBetween: 100,
-		// 	},
-		// },
+	});
+
+	const swiperOrganizations = new Swiper(".swiper-organizations", {
+		enabled: false,
+		spaceBetween: 15,
+		width: 795,
+		navigation: {
+			nextEl: ".slider__right-arrow",
+			prevEl: ".slider__left-arrow",
+		},
+		pagination: {
+			el: ".slider__pagination",
+		},
+		breakpoints: {
+			768: {
+				enabled: true,
+				spaceBetween: 150,
+				width: null
+			},
+		},
 	});
 };
